b31c5bfacc29808bba0ca3cdb8fd916ef220f295
shruti6355 <shruti.praxinfo@gmail.com>
2025-02-11 08:24:56 +0000

    container config done

---
+++ 01_Basics/variable.js
@@ -0,0 +1,19 @@
+const accountId = 123456
+let accountEmail = "shruti@gmail.com"
+var accountPassword = "123456"
+accountCity = "Ahmedabad"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "abc@.com"
+accountPassword = "123456789"
+accountCity = "karnatka"
+
+console.log(accountId);
+
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
