b3c62e07e2fe72320440a944c9ebed5db11fb302
shruti6355 <shruti.praxinfo@gmail.com>
2025-02-17 05:26:31 +0000

    control flow if-else switch terniry done

---
+++ control_flow/switch.js
@@ -0,0 +1,48 @@
+//comparision oprator:"<",">","<=",">=","==","!=","==="
+
+const temp=40
+
+// if(temp<50){
+//     console.log("less then 50");
+    
+// }
+// console.log("temprature is greter then 50")
+
+if(temp===41){
+    console.log("less then 50");
+    
+}
+console.log("temprature is greter then 50")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://medium.com/@glen.gracieux/building-a-simple-html-css-js-website-9cc04a8bd761
---
+++ control_flow/truthy.js
@@ -0,0 +1,30 @@
+// const empmptyobj={}
+// {
+
+
+//     if(Object.keys(empmptyobj).length===0)
+//     {
+//        console.log("object is empty");
+       
+//     }
+
+
+// }
+
+//null or undefine
+let val1;
+// val1=5??10
+// val1=null??10
+// val1=undefined??15
+// console.log(val1);
+val1=null??10??20
+console.log(val1);
+
+//terniry operator
+
+// condition??true:false
+const iceteaprice=100
+iceteaprice <=80?console.log("less than 80 "):console.log("more than 80");
+
+
+
